799ce286363084508dffc27235b579418b30dec6
DavidSM100 <d.s.m.011010@gmail.com>
2023-09-01 08:31:15 -0400

    removed language suport, not necesary for 5 words

--- assets/lang/en.js
+++
@@ -1,4 +0,0 @@
-password.placeholder = "Encryption key";
-text.placeholder = "Text";
-result.placeholder = "Encrypted text";
-document.getElementById("encrypt").textContent = "Encrypt/Decrypt";
--- assets/lang/es.js
+++
@@ -1,4 +0,0 @@
-password.placeholder = "Clave de cifrado";
-text.placeholder = "Texto";
-result.placeholder = "Texto cifrado";
-document.getElementById("encrypt").textContent = "Cifrar/Descifrar";
